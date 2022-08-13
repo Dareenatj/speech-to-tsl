8a1a2a2cc2fad412f897b215ea2feed96bef1dba
J.Dareenat <106639881+Dareenatj@users.noreply.github.com>
2022-08-13 14:37:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("🗣💬 to tsl")
 st.subheader("Translating Thai speech to Thai Sign Language")
-input = st.text_input("Search here")
-
+input_txt = st.text_input("Search here")
+input_sound = st.button("record")
