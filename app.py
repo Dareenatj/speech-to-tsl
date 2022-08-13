2930ceef086fe4b0ef06140cb16273801d553995
J.Dareenat <106639881+Dareenatj@users.noreply.github.com>
2022-08-13 23:10:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with features:
     #"ฉัน":"https://www.engdict.com/vocab/?q=I",
     #"ชื่อ":"https://th.theasianparent.com/hit-trendy-baby-name",
     #"อะไร":"https://www.yuvabadhanafoundation.org/th/%E0%B8%82%E0%B9%88%E0%B8%B2%E0%B8%A7%E0%B8%AA%E0%B8%B2%E0%B8%A3/%E0%B8%9A%E0%B8%97%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%97%E0%B8%B1%E0%B9%88%E0%B8%A7%E0%B9%84%E0%B8%9B/%E0%B9%81%E0%B8%99%E0%B8%B0%E0%B9%81%E0%B8%99%E0%B8%A7-%E0%B8%A7%E0%B8%B1%E0%B8%A2%E0%B8%A3%E0%B8%B8%E0%B9%88%E0%B8%99-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99/"
-    }
+    #}
   #for i in txt:
       #print(words[i])
 #st.button_input("record")
