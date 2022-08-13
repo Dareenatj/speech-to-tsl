b2d0d1186cda1dc088e0015f6338a026e60f368a
J.Dareenat <106639881+Dareenatj@users.noreply.github.com>
2022-08-13 15:56:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import speech_regcognition as sr
+import pythainlp
+from pythainlp import sent_tokenize, word_tokenize
+
 st.title("🗣💬 to tsl")
 st.subheader("Translating Thai speech to Thai Sign Language")
-input_txt = st.text_input("Search here")
-input_sound = st.button("record")
+
+@st.cache
+if st.text_input("Search here", value="", max_chars=None, disabled=False):
+  txt=word_tokenize(value)
+  thisdict={
+    "ฉัน":"https://www.engdict.com/vocab/?q=I",
+    "ชื่อ":"https://th.theasianparent.com/hit-trendy-baby-name",
+    "อะไร":"https://www.yuvabadhanafoundation.org/th/%E0%B8%82%E0%B9%88%E0%B8%B2%E0%B8%A7%E0%B8%AA%E0%B8%B2%E0%B8%A3/%E0%B8%9A%E0%B8%97%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%97%E0%B8%B1%E0%B9%88%E0%B8%A7%E0%B9%84%E0%B8%9B/%E0%B9%81%E0%B8%99%E0%B8%B0%E0%B9%81%E0%B8%99%E0%B8%A7-%E0%B8%A7%E0%B8%B1%E0%B8%A2%E0%B8%A3%E0%B8%B8%E0%B9%88%E0%B8%99-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99/"
+    }
+   print(thisdict(txt))
+#if st.button_input("record"):
+
+
